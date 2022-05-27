92a82a065f2e0968a608b03c9cd54bb04250fa9e
thamyres-Santana <86668892+thamyres-Santana@users.noreply.github.com>
2022-05-26 21:51:14 -0300

    Add files via upload

---
+++ q3.js
@@ -0,0 +1,15 @@
+function basket(string){
+    const jogos = string.split(" ,");
+    const jogos2 = jogos.map(Number); 
+
+    let piorJogo = 0;
+    let recordes = 0;
+    let maiorPontuacao = 0;
+    let menorPontuacao = 0;
+
+    for (i=0; i<jogos2.length; i++){
+        if (i == 0){
+            
+        }
+    }    
+}
